93d71fb03d45be2e5057d2a9d50cb42ce0f1f3c7
Alvee Jalal <alveej520@gmail.com>
2024-09-27 23:31:29 -0400

    some progress in working on authentication

--- webserver/authenticate.php
+++ webserver/authenticate.php
@@ -1,8 +1,21 @@
 <?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('login.php.inc');
+include('testRabbitMQServer.php');
+include('../testRabbitMQClient.php');
+
 
 $username = filter_input(INPUT_POST, 'username');
 $password = filter_input(INPUT_POST, 'password');
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 
+$request['username'] = $username;
+$request['password'] = $password;
+
+
+$request = array();
 
 
 ?>
