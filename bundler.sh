0ba09978058dc4aada5b18c7c925c1e605df7c07
Alvee Jalal <alveej520@gmail.com>
2024-11-22 16:03:13 -0500

    adding version num to filename for bundle

--- bundler.sh
+++ bundler.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 #Create an archive filder, show the zipping process, zip up the file, specify filename of archive to be created
-tar -cvf rabbitmqphp_new.tar $*
+latestVersionNum=$(tail -n 1 "versionTracker.txt")
+newVersionNum=$(($latestVersionNum + 1))
+echo $newVersionNum
+tar -cvf rabbitmqphp_new.${newVersionNum}.tar testdir
 #create scp command to send .tar file over to deployment server
