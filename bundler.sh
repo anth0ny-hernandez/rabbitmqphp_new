548fc908e8eeefe33bbe2b2ce1b52e6d376b371a
Alvee Jalal <alveej520@gmail.com>
2024-11-23 20:03:17 -0500

    scp added to script

--- bundler.sh
+++ bundler.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 #Create an archive filder, show the zipping process, zip up the file, specify filename of archive to be created
-latestVersionNum=$(tail -n 1 "versionTracker.txt")
+
+#find the latest version number tracked and add 1 to it
+latestVersionNum=$(tail -n 1 "../rabbitmqphp_new/versionTracker.txt")
+
+#source reference for converting string to num: https://linuxhandbook.com/bash-convert-string-to-number/
 newVersionNum=$(($latestVersionNum + 1))
 echo $newVersionNum
-tar -cvf rabbitmqphp_new.${newVersionNum}.tar testdir
+
+#append that new version number to tracker
+echo $newVersionNum >> "../rabbitmqphp_new/versionTracker.txt"
+
+#source reference for tarring: https://stackoverflow.com/questions/50338201/how-to-compress-and-tar-a-folder-in-linux
+tar -czf rabbitmqphp_new.${newVersionNum}.tar.gz ../rabbitmqphp_new
+
 #create scp command to send .tar file over to deployment server
+scp rabbitmqphp_new.${newVersionNum}.tar.gz yashmandal@172.22.217.86:/home/yashmandal/git/deployment
