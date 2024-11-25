42251acc6d90faabb433a29fdf8151e8c68719e2
Yash Mandal <ym299@njit.edu>
2024-11-25 12:14:37 -0500

    naming convention

--- triggerDeployment.php
+++ triggerDeployment.php
@@ -18,7 +18,7 @@ if (empty($version_number)) {
 }
 
 // Define the bundle path dynamically based on the version number
-$bundlePath = "/home/yashmandal/git/repo-version-$version_number.tar.gz"; // Adjust if necessary
+$bundlePath = "/home/yashmandal/git/myRepo-$version_number.tar.gz"; // Adjust if necessary
 
 // Send a deployment request to the RabbitMQ server
 try {
