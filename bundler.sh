a444ad0c36ed3592a111c8433598565ff543c12b
Alvee Jalal <alveej520@gmail.com>
2024-11-25 17:02:26 -0500

    fixed naming convention

--- bundler.sh
+++ bundler.sh
@@ -13,8 +13,9 @@ echo $newVersionNum
 echo $newVersionNum >> "../rabbitmqphp_new/versionTracker.txt"
 
 #source reference for tarring: https://stackoverflow.com/questions/50338201/how-to-compress-and-tar-a-folder-in-linux
-tar -czf rabbitmqphp_new.${newVersionNum}.tar.gz $*
+tar -czf myRepo-${newVersionNum}.tar.gz $*
 
 #create scp command to send .tar file over to deployment server
-scp rabbitmqphp_new.${newVersionNum}.tar.gz yashmandal@172.22.217.86:/home/yashmandal/git/deployment
+scp myRepo-${newVersionNum}.tar.gz yashmandal@172.22.217.86:/home/yashmandal/git/deployment
 #scp rabbitmqphp_new.${newVersionNum}.tar.gz alvee-jalal@172.22.87.142:/home/alvee-jalal/git/
+php triggerDeployment.sh
