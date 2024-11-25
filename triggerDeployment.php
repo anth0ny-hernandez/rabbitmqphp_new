5d0ebb868267a72c2faf1a737f5a9645f0481595
Yash Mandal <ym299@njit.edu>
2024-11-25 12:09:13 -0500

    triggerdeploy

--- triggerDeployment.php
+++ triggerDeployment.php
@@ -1,21 +1,36 @@
 <?php
 require_once('rabbitMQLib.inc');
 
-// Configuration for the deployment
-$versionNumber = "v1.0.0"; // Replace with the actual version number
-$bundlePath = "/home/yashmandal/git/my-repo.tar.gz"; // Path to the transferred bundle on the deployment server
+// Path to the version tracker file
+$versionTrackerFile = "/home/yashmandal/test/rabbitmqphp_new/versionTracker.txt"; // Update this path if necessary
+
+// Read the last line of the version tracker file to get the latest version
+$version_number = "";
+if (file_exists($versionTrackerFile)) {
+    $lines = file($versionTrackerFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $version_number = end($lines); // Get the last line
+} else {
+    die("Error: versionTracker.txt file not found.\n");
+}
+
+if (empty($version_number)) {
+    die("Error: Could not determine the latest version from versionTracker.txt.\n");
+}
+
+// Define the bundle path dynamically based on the version number
+$bundlePath = "/home/yashmandal/git/repo-version-$version_number.tar.gz"; // Adjust if necessary
 
 // Send a deployment request to the RabbitMQ server
 try {
-    $client = new rabbitMQClient("deploymentServer.ini", "deploymentServer");
+    $client = new rabbitMQClient("deploymentClient.ini", "deploymentServer");
 
     $request = [
         "type" => "deploy",
-        "version_number" => $versionNumber,
+        "version_number" => "v$version_number", // Example: "v1.0.0"
         "bundle_path" => $bundlePath
     ];
 
-    echo "Sending deployment request...\n";
+    echo "Sending deployment request for version $version_number...\n";
     $response = $client->send_request($request);
 
     if ($response['success']) {
