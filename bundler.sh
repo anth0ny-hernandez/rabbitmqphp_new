05f7211d4280c085f47b166ec5f489dfff6eacd3
Yash Mandal <ym299@njit.edu>
2024-12-07 17:35:41 -0500

    bundler changes

--- bundler.sh
+++ bundler.sh
@@ -1,21 +1,60 @@
 #!/bin/bash
 
-#Create an archive filder, show the zipping process, zip up the file, specify filename of archive to be created
+# Define the path to the version tracker file
+VERSION_TRACKER="../rabbitmqphp_new/versionTracker.txt"
 
-#find the latest version number tracked and add 1 to it
-latestVersionNum=$(tail -n 1 "../rabbitmqphp_new/versionTracker.txt")
+# Check if the version tracker exists
+if [[ ! -f "$VERSION_TRACKER" ]]; then
+    echo "Version tracker file not found: $VERSION_TRACKER"
+    exit 1
+fi
 
-#source reference for converting string to num: https://linuxhandbook.com/bash-convert-string-to-number/
-newVersionNum=$(($latestVersionNum + 1))
-echo $newVersionNum
+# Find the latest version number from the tracker and increment it
+latestVersionNum=$(tail -n 1 "$VERSION_TRACKER")
+newVersionNum=$((latestVersionNum + 1))
 
-#append that new version number to tracker
-echo $newVersionNum >> "../rabbitmqphp_new/versionTracker.txt"
+# Append the new version number to the tracker
+echo $newVersionNum >> "$VERSION_TRACKER"
 
-#source reference for tarring: https://stackoverflow.com/questions/50338201/how-to-compress-and-tar-a-folder-in-linux
-tar -czf myRepo-${newVersionNum}.tar.gz $*
+# Define the output tar file name
+tarFile="myRepo-${newVersionNum}.tar.gz"
+echo "Creating tarball: $tarFile"
 
-#create scp command to send .tar file over to deployment server
-scp myRepo-${newVersionNum}.tar.gz yashmandal@172.22.217.86:/home/yashmandal/git/deployment
-#scp rabbitmqphp_new.${newVersionNum}.tar.gz alvee-jalal@172.22.87.142:/home/alvee-jalal/git/
+# Check if arguments are provided
+if [[ $# -eq 0 ]]; then
+    echo "No specific files provided. Zipping up the entire repository..."
+    repoPath="../rabbitmqphp_new" # Update this to the full path of your repository if needed
+    tar -czf "$tarFile" -C "$repoPath" .
+else
+    echo "Zipping up specified files..."
+    # Bundle the specified files only (without directory structure)
+    tar -czf "$tarFile" --transform='s!.*/!!' "$@"
+fi
+
+if [[ $? -eq 0 ]]; then
+    echo "Successfully created tarball: $tarFile"
+else
+    echo "Error creating tarball. Exiting."
+    exit 1
+fi
+
+# Transfer the tarball to the deployment server
+deploymentPath="/home/yashmandal/git/deployment"
+scp "$tarFile" yashmandal@172.22.217.86:"$deploymentPath"
+
+if [[ $? -eq 0 ]]; then
+    echo "Successfully transferred $tarFile to deployment server."
+else
+    echo "Error transferring tarball to deployment server."
+    exit 1
+fi
+
+# Trigger deployment on the server
 php "/home/yashmandal/test/rabbitmqphp_new/triggerDeployment2.php"
+
+if [[ $? -eq 0 ]]; then
+    echo "Deployment triggered successfully."
+else
+    echo "Error triggering deployment."
+    exit 1
+fi
