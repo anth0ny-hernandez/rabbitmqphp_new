b5447f0ed2e7536c4adcae21acb371d8a2abb3b1
Yash Mandal <ym299@njit.edu>
2024-12-11 12:45:55 -0500

    fixed some triggerDeployment issues

--- triggerDeployment2.php
+++ triggerDeployment2.php
@@ -2,7 +2,7 @@
 require_once('rabbitMQLib.inc');
 
 // Path to the version tracker file
-$versionTrackerFile = "/home/yashmandal/test/rabbitmqphp_new/versionTracker.txt"; // Update this path if necessary
+$versionTrackerFile = "/home/yashmandal/test/rabbitmqphp_new/versionTracker.txt"; // Adjust path if necessary
 
 // Read the last line of the version tracker file to get the latest version
 $version_number = "";
@@ -17,34 +17,24 @@ if (empty($version_number)) {
     die("Error: Could not determine the latest version from versionTracker.txt.\n");
 }
 
-// Prompt the user to select the bundle type
-echo "Select the bundle type for deployment:\n";
-echo "1. frontend\n";
-echo "2. server\n";
-echo "3. dmz\n";
-$bundleType = readline("Enter the number corresponding to your choice: ");
+// Get the bundle type from the latest tar.gz file created
+$bundleDir = "/home/yashmandal/git/deployment";
+$bundleFiles = glob("$bundleDir/*-version-$version_number.tar.gz");
 
-// Determine the bundle type based on the user input
-switch ($bundleType) {
-    case '1':
-        $bundleName = "frontend";
-        break;
-    case '2':
-        $bundleName = "server";
-        break;
-    case '3':
-        $bundleName = "dmz";
-        break;
-    default:
-        die("Invalid choice. Exiting...\n");
+if (empty($bundleFiles)) {
+    die("Error: No bundle found for version $version_number in $bundleDir.\n");
 }
 
-// Define the bundle path dynamically based on the version number and bundle type
-$bundlePath = "/home/yashmandal/git/deployment/${bundleName}-version-${version_number}.tar.gz";
+// Extract the bundle name (frontend, server, dmz) from the file name
+$bundleFileName = basename($bundleFiles[0]);
+preg_match('/^(frontend|server|dmz)-version-/', $bundleFileName, $matches);
 
-if (!file_exists($bundlePath)) {
-    die("Error: The specified bundle file does not exist: $bundlePath\n");
+if (empty($matches[1])) {
+    die("Error: Could not determine the bundle type from the file name.\n");
 }
+
+$bundleType = $matches[1];
+$bundlePath = "$bundleDir/$bundleFileName";
 
 // Send a deployment request to the RabbitMQ server
 try {
@@ -54,10 +44,10 @@ try {
         "type" => "deploy",
         "version_number" => $version_number,
         "bundle_path" => $bundlePath,
-        "bundle_type" => $bundleName // Adding bundle type for better tracking
+        "bundle_type" => $bundleType
     ];
 
-    echo "Sending deployment request for version $version_number ($bundleName)...\n";
+    echo "Sending deployment request for version $version_number ($bundleType)...\n";
     $response = $client->send_request($request);
 
     if ($response['success']) {
